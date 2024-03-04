22b89ca9f04893970b867d8768aaf73e73bee7e4
PUSHKAR TIWARI <74901056+psychiccoder@users.noreply.github.com>
2024-03-04 07:54:44 +0530

    Update main.js
    Using then and catch block instead of async and await

--- main.js
+++ main.js
@@ -3,15 +3,37 @@ const author= document.getElementById("author")
 
 const api_url= "https://api.quotable.io/random"
 
-async function getquote(url){
-    const response =await fetch(url);
-    let data= await response.json();
-    // console.log(data);
-    quote.innerHTML=data.content;
-    author.innerHTML=data.author;
+// async function getquote(url){
+//     const response =await fetch(url);
+//     let data= await response.json();
+//     // console.log(data);
+//     quote.innerHTML=data.content;
+//     author.innerHTML=data.author;
+// }
+
+// getquote(api_url)
+
+
+// With Then And Catch Keyword.
+
+function getquote(url) {
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            quote.innerHTML = data.content;
+            author.innerHTML = data.author;
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
 }
 
-getquote(api_url)
+getquote(api_url);
 
 
 
